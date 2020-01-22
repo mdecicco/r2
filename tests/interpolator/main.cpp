51335337c0909501ba294a28a8ffac7105fbe220
Dr. Michael DeCicco <mdecicco8@gmail.com>
2020-01-22 09:09:30 -0500

    Fixed interpolator test

--- tests/interpolator/main.cpp
+++ tests/interpolator/main.cpp
@@ -5,7 +5,7 @@ using namespace r2;
 int main(int argc, char** argv) {
 	bool done = false;
 
-	Interpolator<f32> i(0, 5, interpolate::easeInOutQuad);
+	interpolator<f32> i(0, 5, interpolate::easeInOutQuad);
 	(i = 1.0f).then([&done]() { done = true; });
 
 	while(!done) {
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
 
 	done = false;
 
-	Interpolator<glm::vec2> v(glm::vec2(0.0f, 0.0f), 5.0f, interpolate::easeOutCubic);
+	interpolator<glm::vec2> v(glm::vec2(0.0f, 0.0f), 5.0f, interpolate::easeOutCubic);
 	(v = glm::vec2(1.0f, 1.0f)).then([&done]() { done = true; });
 
 	while(!done) {
